846e5e1a259066497d1a9ae8ae2a24e38ccbf52e
Dmitriy Suchkov <Account.log@yandex.ru>
2021-06-28 16:23:49 +0300

    tmp commit

--- tmp.py
+++ tmp.py
@@ -7,11 +7,6 @@ from text_tools import split_by_words, calculate_jaundice_rate, get_charged_word
 
 from async_timeout import timeout
 
-def main():
-    d = dict(a=1)
-    print(d)
-
-
 async def test_sanitize():
     resp = requests.get('https://dvmn.org/media/filer_public/51/83/51830f54-7ec7-4702-847b-c5790ed3724c/gogol_nikolay_taras_bulba_-_bookscafenet.txt')
     resp.raise_for_status()
